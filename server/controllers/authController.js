61c5f62ddf585b6d29711cd7598978081873bd73
saile <eliasiv.codes@gmail.com>
2024-03-15 19:26:46 -0700

    implement auth routes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,6 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { StatusCodes } from "http-status-codes";
 import userModel from "../models/userModel.js";
-import { UnauthenticatedError } from "../errors/errors.js";
 
 export const register = async (req, res) => {
   const salt = await bcrypt.genSalt(10);
@@ -13,6 +12,25 @@ export const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ msg: "(Server message) User successfully registered" });
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  const token = jwt.sign({ userId: req.user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRATION,
+  });
 
-export const logout = async (req, res) => {};
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 86400000),
+    secure: process.env.NODE_ENV === "production",
+  });
+
+  res.status(StatusCodes.OK).json({ msg: "(Server message) User successfully logged in" });
+};
+
+export const logout = async (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+
+  res.status(StatusCodes.OK).json({ msg: "(Server message) User successfully logged out" });
+};
