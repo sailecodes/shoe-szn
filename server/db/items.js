d747a6fba68908b0c19d94d8107d8209d1bbc74c
saile <eliasiv.codes@gmail.com>
2024-05-03 00:50:20 -0700

    add getUserItems resolver

--- server/db/items.js
+++ server/db/items.js
@@ -6,14 +6,29 @@ export const getAllItems = async () => {
   return rows;
 };
 
-export const getAllItemsFromCategory = async (category) => {
-  const { rows } = await db.query("SELECT * FROM items where item_category = $1", [category]);
+export const getAllItemsFromCategory = async (itemCategory) => {
+  const { rows } = await db.query("SELECT * FROM items where item_category = $1", [itemCategory]);
 
   return rows;
 };
 
-export const getItem = async (id) => {
-  const { rows } = await db.query("SELECT * FROM items where item_id = $1", [id]);
+export const getItem = async (itemId) => {
+  const { rows } = await db.query("SELECT * FROM items where item_id = $1", [itemId]);
 
   return rows[0];
+};
+
+export const getUserItems = async (userEmail) => {
+  const { rows } = await db.query(
+    `
+      SELECT *
+      FROM users AS u
+      JOIN users_items AS ui ON u.user_email = ui.r_user_email
+      JOIN items AS i ON ui.r_item_id = i.item_id
+      WHERE user_email = $1;
+    `,
+    [userEmail]
+  );
+
+  return rows;
 };
--- server/graphql/resolvers/resolvers.js
+++ server/graphql/resolvers/resolvers.js
@@ -1,11 +1,12 @@
-import { getAllItems, getAllItemsFromCategory, getItem } from "../../db/items.js";
+import { getAllItems, getAllItemsFromCategory, getItem, getUserItems } from "../../db/items.js";
 
 export const resolvers = {
   Query: {
     helloWorld: (_parent, _args, _context) => "Hello World!",
     getAllItems: async (_parent, _args, _context) => await getAllItems(),
-    getAllItemsFromCategory: async (_parent, { category }, _context) => await getAllItemsFromCategory(category),
-    getItem: async (_parent, { id }, _context) => await getItem(id),
+    getAllItemsFromCategory: async (_parent, { itemCategory }, _context) => await getAllItemsFromCategory(itemCategory),
+    getItem: async (_parent, { itemId }, _context) => await getItem(itemId),
+    getUserItems: async (_parent, { userEmail }, _context) => await getUserItems(userEmail),
   },
   User: {},
   Item: {},
