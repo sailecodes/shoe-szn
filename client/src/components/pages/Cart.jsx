fbbbf17e967840de58d857a5305a04fbc6ebd81a
saile <eliasiv.codes@gmail.com>
2024-05-03 20:17:16 -0700

    update shopping cart page

--- client/src/components/pages/Cart.jsx
+++ client/src/components/pages/Cart.jsx
@@ -1,34 +1,58 @@
 import { Link } from "react-router-dom";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 import banana from "../../assets/imgs/VU-Banana-1000x1000.png";
+import { useState } from "react";
 
 const Cart = () => {
+  const [subtotal, setSubtotal] = useState(0);
+
   return (
     <section className="cart">
       <div className="cart--container">
-        <header>Cart</header>
+        <header>Shopping Cart</header>
+        <div className="cart--subheaders">
+          <p>Product</p>
+          <p>Quantity</p>
+          <p>Total</p>
+        </div>
         <div className="cart--items">
           <CartItem />
           <CartItem />
         </div>
+        <div className="cart--subtotal">
+          <p>Subtotal: ${subtotal}</p>
+        </div>
+        <button>Checkout</button>
       </div>
     </section>
   );
 };
 
 const CartItem = () => {
+  const [quantity, setQuantity] = useState(1);
+
   return (
     <div className="cart-item">
       <img src={banana} alt="" />
       <div className="cart-item--information">
-        <p className="cart-item--information-name">Nike Green II</p>
-        <p className="cart-item--information-price">$129.99</p>
-        <p className="cart-item--information-size">Size: 8</p>
+        <div>
+          <p className="cart-item--information-name">Nike Green II</p>
+          <p className="cart-item--information-price">$129</p>
+          <p className="cart-item--information-size">Size: 8</p>
+        </div>
       </div>
-      <div className="cart-item--btns">
-        <Link to="">See item</Link>
-        <button>Remove</button>
+      <div className="cart-item--modifiers">
+        <div className="cart-item--quantity">
+          <button onClick={() => setQuantity(quantity === 1 ? 1 : quantity - 1)}>-</button>
+          <p>{quantity}</p>
+          <button onClick={() => setQuantity(quantity + 1)}>+</button>
+        </div>
+        <button>
+          <FaRegTrashAlt />
+        </button>
       </div>
+      <p className="cart-item--total">$129</p>
     </div>
   );
 };
