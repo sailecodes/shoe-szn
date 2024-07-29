ca99359562b6f09076dc11e1163c46cc4c2c9f18
saile <eliasiv.codes@gmail.com>
2024-07-28 17:32:19 -0700

    minor change

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -3,21 +3,7 @@ import pg from "pg";
 export const db = new pg.Pool({
   host: process.env.PGHOST,
   port: process.env.PGPORT,
-  database: process.env.DATABASE_URL,
+  database: process.env.PGDATABASE,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
 });
-
-// export const db = new pg.Pool(
-//   process.env.NODE_ENV === "development"
-//     ? {
-//         host: process.env.PGHOST,
-//         port: process.env.PGPORT,
-//         database: process.env.PGDATABASE,
-//         user: process.env.PGUSER,
-//         password: process.env.PGPASSWORD,
-//       }
-//     : { connectionString: process.env.DATABASE_URL }
-// );
-
-// export const db = new pg.Pool({ : process.env.DATABASE_URL });
